6f6331cb9070f8fc155f6fb8272d46f08ca06200
韩伟鹏 <hanweipeng@haodf.com>
2020-05-07 16:55:30 +0800

    调整圈速数据

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ $(function () {
   }
   //获取json数据
   $.ajax({
-    url: './speed.json?v=202005071633',
+    url: './speed.json?v=202005071640',
     async: false,
     success: function (res) {
       // console.log(res);
