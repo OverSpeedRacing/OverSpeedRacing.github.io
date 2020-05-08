d7f93cc61d23fb071a04f14e6e3c9715bee446ee
韩伟鹏 <hanweipeng@haodf.com>
2020-05-08 10:28:21 +0800

    上线数据更新脚本

--- update_speed.py
+++ update_speed.py
@@ -7,7 +7,7 @@ if len(sys.argv) < 3:
 u_name = sys.argv[1]
 u_speed = sys.argv[2]
 
-with open('speed.json', 'r') as f:
+with open('speed.json', 'r', encoding='utf-8') as f:
     speeds = json.loads(f.read())
 
 name2speed = dict()
@@ -33,5 +33,11 @@ else:
                 "accelerate": "",
                 "limit": ""}
 
-print (sorted(name2speed.values(), key = lambda i: float(i['speed'])) )
+sorted_speed = sorted(name2speed.values(), key = lambda i: float(i['speed']))
+accelerates = [25, 20, 15, 13, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
+for i, speed in enumerate(sorted_speed):
+    speed['accelerate'] = str(accelerates[i]) if i < 15 else ''
+
+with open('speed.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps(sorted_speed, indent=4, ensure_ascii=False))
